fef0478ed734ca35c237ed30867151b288bc8e52
iananrv <danteiannarvaez@yahoo.com>
2024-11-29 18:10:37 -0500

    Final

---
+++ Assignment 5-1.py
@@ -0,0 +1,68 @@
+# a.
+a = 1
+b = 2 
+c = 5
+while a < c:
+    a = a + 1
+    b = b + c
+    break
+print(a, b, c)
+
+# b. 
+d = 4
+e = 6
+f = 7
+while d > f:
+    d = d + 1
+    e = e - 1
+    break
+print(d, e, f)
+
+# c.
+g = 4
+h = 6
+while g < h:
+    g = g + 1
+    break
+print(g, h)
+
+# d.
+j = 2
+k = 5
+n = 9
+while j < k:
+    m = 6
+    while m < n:
+        print("Goodbye")
+        m = m + 1
+        break
+    j = j + 1 
+    break
+
+# e.
+j = 2
+k = 5 
+m = 6
+n = 9
+while j < k:
+    while m < n:
+        print("Hello")
+        m = m + 1
+        break
+    j = j + 1
+    break
+
+# f.
+p = 2
+q = 4
+
+while p < q:
+    print ("Adios")
+    r = 1
+    while r < q:
+        print ("Adios")
+        r = r + 1
+        break
+    p = p + 1
+    break
+
